8c13930b637d480357d8cbb7ff0ddbe9c7e8e26d
Andrew Buultjens <buultjensa@gmail.com>
2021-02-03 11:01:34 +1100

    Update node-tree-labeler.sh

--- node-tree-labeler.sh
+++ node-tree-labeler.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-INFILE=${1}
-OUTFILE=${2}
+INFILE_TREE=${1}
+NODE_FOFN=${2}
+OUTFILE_TREE=${3}
 
+####################################################################
 
 START=`head -1 $1`
 
-for TAXA in $(cat $1); do
+for TAXA in $(cat ${NODE_FOFN}); do
 
         # generate random prefix for all tmp files
         RAND_1=`echo $((1 + RANDOM % 100))`
@@ -14,24 +16,21 @@ for TAXA in $(cat $1); do
         RAND_3=`echo $((200 + RANDOM % 300))`
         RAND=`echo "${RAND_1}${RAND_2}${RAND_3}"`
 
-	BASE=`grep ^"${TAXA}," test_node_attributes.tr.csv | cut -f 2 -d ','`
+	BASE=`grep ^"${TAXA}," ${INFILE_TREE} | cut -f 2 -d ','`
 
 	if [ "${TAXA}" == "${START}" ]; then	
 		echo "replaced ${TAXA} with ${BASE}"
 		sed "s/${TAXA}\[/${BASE}\[/g" < node_labelled_nexus.tre > ${RAND}_TMP.nwk
-#		awk '{gsub("${TAXA}[", "${BASE}[", $0); print}' < node_labelled_nexus.tre > ${RAND}_TMP.nwk
-
 	else
 		echo "replaced ${TAXA} with ${BASE}"
 		sed "s/${TAXA}\[/${BASE}\[/g" < ${OLD_RAND}_TMP.nwk > ${RAND}_TMP.nwk
-#		awk '{gsub("${TAXA}[", "${BASE}[", $0); print}' < ${OLD_RAND}_TMP.nwk > ${RAND}_TMP.nwk
 	fi
 	
 	OLD_RAND=${RAND}
 	
 done
 
-echo "wrote outfile as ${OUTFILE}"
-mv ${RAND}_TMP.nwk ${OUTFILE}
+echo "wrote outfile as ${OUTFILE_TREE}"
+mv ${RAND}_TMP.nwk ${OUTFILE_TREE}
 
 rm *_TMP.nwk
