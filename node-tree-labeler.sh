1ea00473d1785cdbaf3f74d7ad6c0c222153e566
Andrew Buultjens <buultjensa@gmail.com>
2021-02-03 11:30:22 +1100

    Add files via upload

--- node-tree-labeler.sh
+++ node-tree-labeler.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+# $ sh sh.sh INFILE_TREE NODE_FOFN ASR_MFA SITE OUTFILE
+
 INFILE_TREE=${1}
 NODE_FOFN=${2}
-DB=${3}
-OUTFILE=${4}
+ASR_MFA=${3}
+SITE=${4}
+OUTFILE=${5}
 
+# generate random prefix for all tmp files
+RAND_1=`echo $((1 + RANDOM % 100))`
+RAND_2=`echo $((100 + RANDOM % 200))`
+RAND_3=`echo $((200 + RANDOM % 300))`
+DB_RAND=`echo "${RAND_1}${RAND_2}${RAND_3}"`
+
+grep -v ">" ${ASR_MFA} | cut -b${SITE} > ${DB_RAND}_tmp.seq
+grep ">" ${ASR_MFA} | tr -d '>' > ${DB_RAND}_tmp_col.txt
+paste ${DB_RAND}_tmp_col.txt ${DB_RAND}_tmp.seq | tr '\t' ',' > ${DB_RAND}_DB.csv
 
 START=`head -1 ${NODE_FOFN}`
 
@@ -16,7 +28,7 @@ for TAXA in $(cat ${NODE_FOFN}); do
         RAND_3=`echo $((200 + RANDOM % 300))`
         RAND=`echo "${RAND_1}${RAND_2}${RAND_3}"`
 
-	BASE=`grep ^"${TAXA}," ${DB} | cut -f 2 -d ','`
+	BASE=`grep ^"${TAXA}," ${DB_RAND}_DB.csv | cut -f 2 -d ','`
 
 	if [ "${TAXA}" == "${START}" ]; then	
 		echo "replaced ${TAXA} with ${BASE}"
@@ -34,3 +46,4 @@ echo "wrote outfile as ${OUTFILE}"
 mv ${RAND}_TMP.nwk ${OUTFILE}
 
 rm *_TMP.nwk
+rm *${DB_RAND}*
